a0192e691237b99f1a5e4573c288e838d54ba8f2
Jesse-Tingle <jtingle0@gmail.com>
2020-01-17 21:21:37 -0500

    to do list completed - added functionality to add items, remove individual items, and clear entire list

--- my-app/src/components/ToDoList.js
+++ my-app/src/components/ToDoList.js
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useReducer, useRef } from "react";
 
 function ToDoList() {
-  return <div></div>;
+  const inputRef = useRef();
+
+  const [items, dispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case "add":
+        return [
+          ...state,
+          {
+            id: state.length,
+            name: action.name
+          }
+        ];
+      case "remove":
+        return state.filter((_, index) => index !== action.index);
+      case "clear":
+        return [];
+      default:
+        return state;
+    }
+  }, []);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    dispatch({
+      type: "add",
+      name: inputRef.current.value
+    });
+    inputRef.current.value = "";
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input ref={inputRef} />
+      </form>
+
+      <button onClick={() => dispatch({ type: "clear" })}>Clear</button>
+
+      <ul>
+        {items.map((item, index) => (
+          <li key={item.id}>
+            {item.name}{" "}
+            <button onClick={() => dispatch({ type: "remove", index })}>
+              X
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ToDoList;
